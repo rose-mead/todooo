54b1c9f69b9b14fa731e670da664557d3ad40aa6
rose-mead <rose-mead@users.noreply.github.com>
2021-05-11 19:44:34 +1200

    modify seeds

--- server/db/seeds/tasks.js
+++ server/db/seeds/tasks.js
@@ -5,9 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('tasks').insert([
-        {id: 1, name: 'eat', priority: 1, details: 'Eat lots', status: 1},
-        {id: 2, name: 'code', priority: 2, details: 'Remember to commit', status: 0},
-        {id: 3, name: 'sleep', priority: 1, details: 'at least 8 hours', status: 0}
+        {id: 1, name: 'eat', priority: 1, details: 'Eat healthy food', status: 1},
+        {id: 2, name: 'sleep', priority: 2, details: 'Get 8 hours of sleep', status: 0},
+        {id: 3, name: 'code', priority: 1, details: 'Code every day', status: 0},
+        {id: 4, name: 'repeat', priority: 1, details: '', status: 0}
       ]);
     });
 };
